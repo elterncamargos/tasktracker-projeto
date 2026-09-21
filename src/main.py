03b8aeed19dfbcfd8cd0050170ccae29b53dc8bf
ELTERN CAMARGOS VIEIRA <eltern.camargos@sempreceub.com>
2026-09-21 16:22:48 -0300

    Adiciona menu principal do TaskTracker

---
+++ src/main.py
@@ -0,0 +1,13 @@
+while True:
+    print("\n============================")
+    print("       TASKTRACKER")
+    print("============================")
+    print("1 - Cadastrar nova tarefa")
+    print("2 - Visualizar tarefas cadastradas")
+    print("3 - Sair da aplicação")
+
+    opcao = input("\nEscolha uma opção: ")
+
+    if opcao == "3":
+        print("\nAplicação encerrada.")
+        break
